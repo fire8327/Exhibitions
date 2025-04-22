f9f46c03bfa3853054291db8c24dc7a8e952534e
fire8327 <fire83274@gmail.com>
2025-04-22 13:03:14 +0300

    upd ui

--- pages/profile/add-device.vue
+++ pages/profile/add-device.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="flex flex-col gap-6">
-        <p class="mainHeading">Добавление устройства</p>
+        <div class="flex md:items-center md:justify-between gap-4 max-md:flex-col w-full">
+            <p class="mainHeading">Добавление устройства</p>
+            <NuxtLink to="/profile" class="max-md:order-first px-4 py-2 border border-cyan-500 text-cyan-500 rounded-full w-[160px] text-center transition-all duration-500 hover:text-white hover:bg-cyan-500">Назад</NuxtLink>
+        </div>
         <FormKit type="form" :actions="false" messages-class="hidden" form-class="w-full flex flex-col gap-6 items-center justify-center text-[#1C1C1C]">
             <FormKit v-model="deviceForm.name" validation="required" messages-class="text-[#E9556D] font-mono" type="text" placeholder="Наименование устройства" name="Наименование устройства" outer-class="w-full md:w-2/3 lg:w-1/2" input-class="focus:outline-none px-4 py-2 bg-white rounded-xl border border-transparent w-full transition-all duration-500 focus:border-cyan-500 shadow-md"/>
             <FormKit v-model="deviceForm.desc" validation="required" messages-class="text-[#E9556D] font-mono" type="textarea" placeholder="Описание устройства" name="Описание устройства" outer-class="w-full md:w-2/3 lg:w-1/2" input-class="focus:outline-none px-4 py-2 bg-white rounded-xl border border-transparent w-full transition-all duration-500 focus:border-cyan-500 shadow-md"/>
