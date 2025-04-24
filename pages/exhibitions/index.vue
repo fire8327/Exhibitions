7d69d0def2a3b5660d881110b382cb743ecf5399
fire8327 <fire83274@gmail.com>
2025-04-24 10:46:05 +0300

    upd exhibitions

--- pages/exhibitions/index.vue
+++ pages/exhibitions/index.vue
@@ -1,18 +1,18 @@
 <template>
       <div class="flex flex-col gap-6">
         <p class="mainHeading">Все выставки</p>
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            <NuxtLink to="/" v-for="exhibition in exhibitions"
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6" v-if="data">
+            <NuxtLink to="/" v-for="exhibition in data"
                 class="group bg-gradient-to-b from-[#2C2C2C]/90 to-[#1C1C1C] rounded-2xl overflow-hidden transition-all duration-500 shadow-xl">
                 <div class="relative overflow-hidden">
-                    <img :src="exhibition.image" :alt="exhibition.title"
+                    <img :src="`https://qwcdjqzfdpcefrsxdktn.supabase.co/storage/v1/object/public/files/images/${exhibition.image}`" :alt="exhibition.name"
                         class="w-full aspect-video object-cover brightness-75 transition-all duration-500 group-hover:brightness-100 group-hover:scale-105" />
                 </div>
                 <div class="p-6 flex flex-col gap-2">
-                    <span class="mainHeading">{{ exhibition.title }}</span>
-                    <p class="font-light line-clamp-2">
-                        {{ exhibition.description }}
-                    </p>
+                    <span class="text-xl font-semibold font-mono text-white">{{ exhibition.name }}</span>
+                    <span class="font-light line-clamp-2">
+                        {{ exhibition.desc }}
+                    </span>
                     <span
                         class="flex bg-gradient-to-r from-cyan-500 to-cyan-600 text-white px-4 py-1.5 rounded-full font-medium transition-all duration-500 group-hover:-translate-y-1 group-hover:shadow-md w-fit">
                         Подробнее
@@ -20,53 +20,19 @@
                 </div>
             </NuxtLink>
         </div>
+        <div class="flex flex-col items-center justify-center text-center grow gap-6">
+          <p class="mainHeading">Выставок пока нет</p>
+          <NuxtLink to="/" class="mx-auto px-4 py-2 border border-cyan-500 text-cyan-500 rounded-full w-[160px] text-center transition-all duration-500 hover:text-white hover:bg-cyan-500">На главную</NuxtLink>
+        </div>
     </div>
 </template>
 
 <script setup>
-// Рандомные данные для выставок
-const exhibitions = [
-  {
-    id: '1',
-    title: 'Tech Expo 2025',
-    description: 'Новейшие смартфоны, ноутбуки и умные гаджеты от ведущих брендов.',
-    image: 'https://img.freepik.com/free-photo/person-using-ar-technology-their-daily-occupation_23-2151137526.jpg?t=st=1745230247~exp=1745233847~hmac=1cb656314088a7141ba658d02befb10c1c3e278a6bf1bf7cc675dbc137bf7bc9&w=996',
-    slug: 'tech-expo-2025',
-  },
-  {
-    id: '2',
-    title: 'Gadget Showcase',
-    description: 'Инновационные устройства для дома и работы, представленные в AR.',
-    image: 'https://img.freepik.com/free-photo/person-using-ar-technology-their-daily-occupation_23-2151137526.jpg?t=st=1745230247~exp=1745233847~hmac=1cb656314088a7141ba658d02befb10c1c3e278a6bf1bf7cc675dbc137bf7bc9&w=996',
-    slug: 'gadget-showcase',
-  },
-  {
-    id: '1',
-    title: 'Tech Expo 2025',
-    description: 'Новейшие смартфоны, ноутбуки и умные гаджеты от ведущих брендов.',
-    image: 'https://img.freepik.com/free-photo/person-using-ar-technology-their-daily-occupation_23-2151137526.jpg?t=st=1745230247~exp=1745233847~hmac=1cb656314088a7141ba658d02befb10c1c3e278a6bf1bf7cc675dbc137bf7bc9&w=996',
-    slug: 'tech-expo-2025',
-  },
-  {
-    id: '2',
-    title: 'Gadget Showcase',
-    description: 'Инновационные устройства для дома и работы, представленные в AR.',
-    image: 'https://img.freepik.com/free-photo/person-using-ar-technology-their-daily-occupation_23-2151137526.jpg?t=st=1745230247~exp=1745233847~hmac=1cb656314088a7141ba658d02befb10c1c3e278a6bf1bf7cc675dbc137bf7bc9&w=996',
-    slug: 'gadget-showcase',
-  },
-  {
-    id: '1',
-    title: 'Tech Expo 2025',
-    description: 'Новейшие смартфоны, ноутбуки и умные гаджеты от ведущих брендов.',
-    image: 'https://img.freepik.com/free-photo/person-using-ar-technology-their-daily-occupation_23-2151137526.jpg?t=st=1745230247~exp=1745233847~hmac=1cb656314088a7141ba658d02befb10c1c3e278a6bf1bf7cc675dbc137bf7bc9&w=996',
-    slug: 'tech-expo-2025',
-  },
-  {
-    id: '2',
-    title: 'Gadget Showcase',
-    description: 'Инновационные устройства для дома и работы, представленные в AR.',
-    image: 'https://img.freepik.com/free-photo/person-using-ar-technology-their-daily-occupation_23-2151137526.jpg?t=st=1745230247~exp=1745233847~hmac=1cb656314088a7141ba658d02befb10c1c3e278a6bf1bf7cc675dbc137bf7bc9&w=996',
-    slug: 'gadget-showcase',
-  }
-]
+/* подключение БД */
+const supabase = useSupabaseClient()
+
+const { data, error } = await supabase
+.from('exhibitions')
+.select()
+.eq('is_published', true)
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -28,14 +28,14 @@
             <NuxtLink to="/" v-for="exhibition in exhibitions"
                 class="group bg-gradient-to-b from-[#2C2C2C]/90 to-[#1C1C1C] rounded-2xl overflow-hidden transition-all duration-500 shadow-xl">
                 <div class="relative overflow-hidden">
-                    <img :src="exhibition.image" :alt="exhibition.title"
+                    <img :src="`https://qwcdjqzfdpcefrsxdktn.supabase.co/storage/v1/object/public/files/images/${exhibition.image}`" :alt="exhibition.title"
                         class="w-full aspect-video object-cover brightness-75 transition-all duration-500 group-hover:brightness-100 group-hover:scale-105" />
                 </div>
                 <div class="p-6 flex flex-col gap-2">
-                    <span class="mainHeading">{{ exhibition.title }}</span>
-                    <p class="font-light line-clamp-2">
-                        {{ exhibition.description }}
-                    </p>
+                    <span class="text-xl font-semibold font-mono text-white">{{ exhibition.name }}</span>
+                    <span class="font-light line-clamp-2">
+                        {{ exhibition.desc }}
+                    </span>
                     <span
                         class="flex bg-gradient-to-r from-cyan-500 to-cyan-600 text-white px-4 py-1.5 rounded-full font-medium transition-all duration-500 group-hover:-translate-y-1 group-hover:shadow-md w-fit">
                         Подробнее
@@ -67,24 +67,6 @@
 </template>
 
 <script setup>
-// Рандомные данные для выставок
-const exhibitions = [
-  {
-    id: '1',
-    title: 'Tech Expo 2025',
-    description: 'Новейшие смартфоны, ноутбуки и умные гаджеты от ведущих брендов.',
-    image: 'https://img.freepik.com/free-photo/person-using-ar-technology-their-daily-occupation_23-2151137526.jpg?t=st=1745230247~exp=1745233847~hmac=1cb656314088a7141ba658d02befb10c1c3e278a6bf1bf7cc675dbc137bf7bc9&w=996',
-    slug: 'tech-expo-2025',
-  },
-  {
-    id: '2',
-    title: 'Gadget Showcase',
-    description: 'Инновационные устройства для дома и работы, представленные в AR.',
-    image: 'https://img.freepik.com/free-photo/person-using-ar-technology-their-daily-occupation_23-2151137526.jpg?t=st=1745230247~exp=1745233847~hmac=1cb656314088a7141ba658d02befb10c1c3e278a6bf1bf7cc675dbc137bf7bc9&w=996',
-    slug: 'gadget-showcase',
-  },
-]
-
 /* проверка роли и */
 const userStore = useUserStore()
 const { id:userId, role, authenticated } = useUserStore()
@@ -134,10 +116,25 @@ const loadUsers = async () => {
 }
 
 
+/* загрузка выставок */
+const exhibitions = ref([])
+const loadExhibitions = async () => {
+  const { data, error } = await supabase
+  .from('exhibitions')
+  .select('*')
+  .eq('is_published', true)
+
+  if(data) {
+    exhibitions.value = data
+  }
+}
+
+
 /* первоначальная загрузка */
 onMounted(() => {
   loadCategories()
   loadDevices()
   loadUsers()
+  loadExhibitions()
 })
 </script>
